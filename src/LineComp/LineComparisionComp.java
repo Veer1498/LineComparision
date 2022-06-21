5a077a98ff89d276c93e8d965ec0d3b3bbba37b7
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-21 23:03:34 +0530

    Welcome Message

---
+++ src/LineComp/LineComparisionComp.java
@@ -0,0 +1,11 @@
+package LineComp;
+
+public class LineComparisionComp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+
+	}
+
+}
